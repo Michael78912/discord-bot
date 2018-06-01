8dfe0da96518dcd4727646b3534221352100910f
Michael Gill <michaelgill8123@outlook.com>
2018-05-31 20:36:46 -0700

    added some things

--- bot.py
+++ bot.py
@@ -1,40 +1,119 @@
 import pprint
 import time as _time
 import traceback
+import os
+import shutil
+import string
+import random
+os.chdir(os.path.dirname(__file__))
 
 import discord
 from discord.ext import commands
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import wikipedia
 
 from stdabsorb import StdAbsorber
-bot = commands.Bot(command_prefix=">", description='A bot that greets the user back.')
+
+bot = commands.Bot(command_prefix=">", description='A bot that does absolutely nothing useful.')
 client = discord.Client()
 
+BAD_COMMANDS = ['os.system', 'os.chdir', 'os.listdir', 'os.remove', 'import shutil',
+                '__import__(\'shutil\')', 'exit', 'Exit', 'quit', 'KeyboardInterrupt']
 
 @bot.command()
 async def snowman(ctx):
+    """
+    send a snowman
+    """
     await ctx.send("lol you expected, a snowman, but i can't do that, so have an 8.")
 
 
 @bot.command()
-async def mention(ctx):
-    await ctx.send(ctx.author.mention)
+async def mention(ctx, do_it='yes'):
+    """
+    mention the sender
+    if do_it == 'yes': actually do it.
+    """
+    if do_it == 'yes':
+        await ctx.send(ctx.author.mention)
 
 @bot.command()
 async def time_in_kelowna(ctx):
+    """
+    send the current time in Kelowna, BC, Canada.
+    """
     await ctx.send(_time.asctime())
 
 @bot.command()
-async def see_kelowna(ctx):
-    # server = client.get_server()
+async def see_kelowna(ctx, option='--embed'):
+    """
+    send a picture of the current view of kelowna, BC Canada.
+
+    >see_kelowna --url: sends the link to the image instead of embeding an actual file
+    >see_kelowna --embed / >see_kelowna: embeds the file
+    """
     soup = BeautifulSoup(urlopen('https://www.castanet.net/scenic-web-cams/camera/7/', ))
     division = soup.find('img', {'id': 'camImage', 'alt': 'Downtown Kelowna'})
-    img = division.get_attribute_list('src')[0]
-    await ctx.send('image of kelowna: ' + img)
+    img_url = division.get_attribute_list('src')[0]
+    channel = ctx.channel
+    if option == '--url':
+        await ctx.send('image of kelowna: ' + img_url)
+        print(img_url)
+
+    elif option == '--embed':
+        with open('kelowna.png', 'wb') as f:
+            shutil.copyfileobj(urlopen(img_url), f)
+        await channel.send(file=discord.File('kelowna.png'))
 
 @bot.command()
-async def python_exec(ctx, code):
+async def wiki_summary(ctx, item):
+    """
+    search wikipedia for item and output summary.
+    if no item is found, will output "no summary for [item] found"
+    """
+    try:
+        await ctx.send(wikipedia.summary(item))
+    except:
+        await ctx.send("no summary found for %a found." % item)
+            
+
+class Joker:
+    def __init__(self):
+        parser = BeautifulSoup(
+            urlopen('https://michael78912.github.io/puns.html'))
+        self.division = parser.p.get_text().split('\n')[2:-1]
+        self.joke = random.choice(self.division).strip()
+
+    def say_joke(self):
+        print(self.joke)
+
+    def new_joke(self):
+        self.joke = random.choice(self.division)
+
+
+
+
+
+
+@bot.command()
+async def python_exec(ctx, *args: str):
+    """
+    executes python code, and outputs as a message.
+    """
+    print(args)
+
+    code = ' '.join(args)
+    if code.startswith('`'):
+        code = code.strip('`')
+        code = args.join('\n')
+        print(code)
+    for cmd in BAD_COMMANDS:
+        if cmd in code:
+            await ctx.send(ctx.author.mention + ' I can\'t let you execute %a, for the health of my own\
+computer. this is because you could do something on MY computer that I don\'t want you to do' % code)
+            return
+    
     stdout = StdAbsorber('stdout')
     stdout.set_file()
     try:
@@ -47,29 +126,27 @@ async def python_exec(ctx, code):
         return_str = get_details(e)
         print(return_str)
 
-    return_str = '```python\n' + return_str + '```'
+    return_str = '```python\n' + return_str + '```' if return_str != '' else 'No output'
 
 
     await ctx.send(return_str)
-    
+
+@python_exec.error
+async def pyexc_error(ctx, error):
+    pass
 
 @bot.command()
-async def get_help(ctx):
-    help_str = """```RandomBot- a completely random and useless bot
-It is still being developed, so it can become even more useless in
-the long run.
+async def joke(ctx):
+    """
+    sends a random joke from https://michael78912.github.io/puns.html
+    """
+    await ctx.send(Joker().joke)
 
-commands:
-
-    snowman: display a snowman
-    mention: mention the sender
-    time_in_kelowna: send the current time in Kelowna, BC, Canada.
-    see_kelowna: send a picture of the current webcam shot of kelowna, as viewed from Knox mountain.
-    python_exec: executes python code, and outputs as a message.
-
-    more coming soon! this bot will only work when I'm online, so have fun.```
-"""
-    await ctx.send(help_str)
+@bot.command()
+async def test(ctx):
+    channel = ctx.channel
+    with open('test.png', 'rb') as f:
+        await channel.send(file=discord.File('test.png'))
 
 def get_details(exception):
     """
